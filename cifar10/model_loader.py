248ebca6a49b4d9e58e52d34914551f2708f1d0a
Guozhang Chen <ifgovh@163.com>
2019-09-15 20:19:12 +1000

    add alexnet, fc, and simple net

--- cifar10/model_loader.py
+++ cifar10/model_loader.py
@@ -3,6 +3,7 @@ import torch, torchvision
 import cifar10.models.vgg as vgg
 import cifar10.models.resnet as resnet
 import cifar10.models.densenet as densenet
+import cifar10.models.Alexnet as Alexnet
 
 # map between model name and function
 models = {
@@ -35,6 +36,9 @@ models = {
     'wrn56_8_noshort'       : resnet.WRN56_8_noshort,
     'wrn110_2_noshort'      : resnet.WRN110_2_noshort,
     'wrn110_4_noshort'      : resnet.WRN110_4_noshort,
+    'alex'                  : Alexnet.alexnet,
+    'fc'                    : Alexnet.fc,
+    'simplenet'             : Alexnet.simplenet,
 }
 
 def load(model_name, model_file=None, data_parallel=False):
