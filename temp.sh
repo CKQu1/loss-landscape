3c9991aa13ae1809887ead6b737d904a89b75f4e
Guozhang Chen <ifgovh@163.com>
2019-10-01 10:41:35 +1000

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,43 @@
+python plot_trajectory.py --model resnet56_noshort --model_folder ./trained_nets/resnet56_noshort_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
 
+python plot_trajectory.py --model resnet56_noshort --model_folder ./trained_nets/resnet56_noshort_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
 
-python plot_trajectory.py --model alex --model_folder ./trained_nets/alex_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1400 --ignore biasbn
+python plot_trajectory.py --model resnet56_noshort --model_folder ./trained_nets/resnet56_noshort_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet56 --model_folder ./trained_nets/resnet56_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet56 --model_folder ./trained_nets/resnet56_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet110_noshort --model_folder ./trained_nets/resnet110_noshort_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet110_noshort --model_folder ./trained_nets/resnet110_noshort_sgd_lr\=0.1_bs\=256_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet110_noshort --model_folder ./trained_nets/resnet110_noshort_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet110 --model_folder ./trained_nets/resnet110_sgd_lr\=0.1_bs\=256_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model resnet110 --model_folder ./trained_nets/resnet110_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 500 --ignore biasbn
+
+python plot_trajectory.py --model alex --model_folder ./trained_nets/alex_noshort_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model alex --model_folder ./trained_nets/alex_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model fc3 --model_folder ./trained_nets/fc3_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model fc20 --model_folder ./trained_nets/fc20_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model fc56 --model_folder ./trained_nets/fc56_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet20 --model_folder ./trained_nets/resnet20_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet20 --model_folder ./trained_nets/resnet20_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet20 --model_folder ./trained_nets/resnet20_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet20_noshort --model_folder ./trained_nets/resnet20_noshort_sgd_lr\=0.1_bs\=1024_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet20_noshort --model_folder ./trained_nets/resnet20_noshort_sgd_lr\=0.1_bs\=512_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet14 --model_folder ./trained_nets/resnet14_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
+
+python plot_trajectory.py --model resnet14_noshort --model_folder ./trained_nets/resnet14_noshort_sgd_lr\=0.1_bs\=128_wd\=0_mom\=0_save_epoch\=1/ --dir_type weights --max_epoch 1300 --ignore biasbn
